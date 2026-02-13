4905360020ffc99558d785a62319de527c59921a
mustafa devrim yıldız <mustafadevrim23@gmail.com>
2026-02-13 16:21:10 +0300

    Create product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,8 @@
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+
+  Product({required this.id, required this.name, required this.description, required this.price});
+}
